706c5c414eb99b92959f9dcb54c3fe29cc099f33
rantegu <leroyoy7@gmail.com>
2021-01-20 18:26:00 +0100

    add animation text

---
+++ app.js
@@ -0,0 +1,26 @@
+const sectionWrite = document.querySelectorAll(".descpo");
+
+const options = {
+  root: null,
+  threshold: 1,
+  rootMargin: "10px",
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+options);
+
+sectionWrite.forEach((section) => {
+  appearOnScroll.observe(section);
+});
